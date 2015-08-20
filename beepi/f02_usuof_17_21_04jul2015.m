d65a93aeb8c6e129645e607c60042168678a4937
vkedco <vladimir.kulyukin@gmail.com>
2015-08-19 18:17:38 -0600

    Create f02_usuof_17_21_04jul2015.m

---
+++ beepi/f02_usuof_17_21_04jul2015.m
@@ -0,0 +1,345 @@
+%%% FFT Analysis of F02_USUOF_25Jul2015
+%%% 17:00pm - 21:00pm, 04 July 2015
+%%% Vladimir Kulyukin
+
+%%% Need to change this path
+dir = 'C:\Users\Vladimir\research\audio_files\wav\beepi\f02_usuof_25jul2015\17_21_04jul\';
+file = strcat(dir, '2015-07-04_17-23-44.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_17-23-44.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_17-23-44.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_17-43-44.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_17-43-44.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_17-43-44.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_18-03-44.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_18-03-44.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_18-03-44.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_18-23-45.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_18-23-45.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_18-23-45.wav');
+
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_18-43-45.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_18-43-45.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_18-43-45.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_19-03-45.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_19-03-45.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_19-03-45.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_19-43-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_19-43-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_19-43-46.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_20-03-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_20-03-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_20-03-46.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_20-23-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_20-23-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_20-23-46.wav');
+
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_20-43-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_20-43-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_20-43-46.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_21-03-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_21-03-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_21-03-46.wav');
+
+%%% Need to change this path
+file = strcat(dir, '2015-07-04_21-23-46.wav');
+[y,Fs] = audioread(file);
+
+Nsamps = length(y);
+t = (1/Fs)*(1:Nsamps);          %Prepare time data for plot
+
+%Do Fourier Transform
+y_fft = abs(fft(y));            %Retain Magnitude
+y_fft = y_fft(1:Nsamps/2);      %Discard Half of Points
+f = Fs*(0:Nsamps/2-1)/Nsamps;   %Prepare freq data for plot
+
+%Plot Sound File in Time Domain
+figure;
+plot(t, y);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('F02\_USUOF: 2015-07-04\_21-23-46.wav in Time Domain');
+
+%Plot Sound File in Frequency Domain
+figure;
+plot(f, y_fft);
+xlim([0 1500]);
+ylim([0 150]);
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+title('F02\_USUOF: 2015-07-04\_21-23-46.wav');
