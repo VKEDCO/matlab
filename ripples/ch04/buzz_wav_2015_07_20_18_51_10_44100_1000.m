6e7be8cf2d7fd7a2f93c11559b83c422524b3999
vkedco <vladimir.kulyukin@gmail.com>
2016-04-28 19:32:54 +0000

    Create buzz_wav_2015_07_20_18_51_10_44100_1000.m

---
+++ ripples/ch04/buzz_wav_2015_07_20_18_51_10_44100_1000.m
@@ -0,0 +1,10 @@
+dir = "C:\\Users\\vladimir\\research\\audio_files\\wav\\beepi\\garland_07jul15_04aug15\\";
+y_buzz_wav = load(strcat(dir, "2015-07-20_18-51-10_44100_1000.txt"));
+y_buzz_wav = rotdim(y_buzz_wav);
+n = length(y_buzz_wav);
+t = (1.0/(n-1))*[0:n-1];
+figure;
+plot(t, y_buzz_wav);
+xlim([0 1]);
+plot(t, y_buzz_wav);
+title('Buzz Wav');
